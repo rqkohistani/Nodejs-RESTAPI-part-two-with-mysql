9ad7b88d01db4d611f96e2f8352f65757d2ef1c3
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-24 09:22:50 +0200

    adds admin schemas

---
+++ modules/admin/schemas/index.js
@@ -0,0 +1,5 @@
+import createAdminSchema from './createAdmin.schema.json';
+import updateAdminSchema from './updateAdmin.schema.json';
+import deleteAdminSchema from './deleteAdmin.schema.json';
+
+export { createAdminSchema, updateAdminSchema, deleteAdminSchema };
