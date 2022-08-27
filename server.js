e0cb90a2bfd142618b17fc3bafa2e391e19a7f03
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-27 16:30:49 +0200

    adds main routes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes, { notFoundRoute } from './routes';
 import errorHandler from './errorHandler';
 
 const port = process.env.PORT || 4000;
+const __dirname = path.resolve();
+
 const app = express();
 
 const config = () => {
@@ -15,6 +18,10 @@ const config = () => {
 };
 
 const serve = async () => {
+  // Main page route handler function - GET request
+  app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+  });
   app.use('/api/v2/', routes());
   app.use(notFoundRoute);
   app.use(errorHandler);
